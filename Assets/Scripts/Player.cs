6a96d8187b2b1d00f119c82c91d09bf3f1172d5c
bli-desktop <bli@trinity.edu>
2018-08-31 00:54:34 -0500

    bow!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,28 +4,56 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    Vector3 mousePos;
     float bowChargeLimit = 1f;
-    float bowCharge = 0f;
-    List<Arrow> arrows;
+    float chargeTime = 0f;
+    List<GameObject> arrows;
+    public GameObject arrowPrefab;
+    GameObject arrow;
     Rigidbody2D rb;
     public float speed = 2f;
 
     void Awake() {
-        rb = GetComponent<Rigidbody2D>();    
+        rb = GetComponent<Rigidbody2D>();
+        arrows = new List<GameObject>();
     }
-    void Start () {
-		
-	}
-	
+
+    void GetArrow() {
+        foreach(GameObject a in arrows) {
+            if (!a.activeSelf) {
+                a.SetActive(true);
+                arrow = a;
+                break;
+            }
+        }
+        if (arrow == null) {
+            GameObject go = Instantiate(arrowPrefab, gameObject.transform);
+            arrows.Add(go);
+            arrow = go;
+        }
+        arrow.transform.parent = transform;
+        arrow.transform.localPosition= new Vector3(0.15f, 0f, 0f);
+    }
+
 	void Update () {
-        //if (Input.GetButtonDown("bow")) {
-        //    bowCharge += Time.smoothDeltaTime;
-        //}
-        //if (Input.GetButtonUp("bow")) {
-        //    float charge = Mathf.Min(bowCharge, bowChargeLimit);
-        //    float range = charge;
-        //    float dmg = 2f * charge;
-        //}
+        mousePos = Input.mousePosition;
+        mousePos.z = 0f;
+        Vector3 mouseDir = Camera.main.ScreenToWorldPoint(mousePos) - transform.position;
+        float angle = Mathf.Atan2(mouseDir.y, mouseDir.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 10f*Time.deltaTime);
+
+        if (Input.GetButtonDown("bow")) {
+            chargeTime = Time.time;
+            GetArrow();
+        }
+        if (Input.GetButtonUp("bow")) {
+            float charge = Mathf.Min(Time.time-chargeTime, bowChargeLimit);
+            float range = charge;
+            float dmg = 2f * charge;
+            arrow.GetComponent<Rigidbody2D>().AddForce(new Vector2(mouseDir.x, mouseDir.y).normalized * 500f);
+            arrow.transform.parent = null;
+        }
 
         float hor = Input.GetAxis("Horizontal");
         float vert = Input.GetAxis("Vertical");
