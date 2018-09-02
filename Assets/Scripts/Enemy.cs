0b7d3c518ebac2aa3894ab17dc5184becd9175f4
bli-desktop <bli@trinity.edu>
2018-09-01 19:22:05 -0500

    enemy unfinished

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-    
+    Hero target;
     public Map map = Map.someMap;
     public float knockDuration = 0.5f;
+    public float knockMult = 2f;
+    public float moveSpeed = 1f;
     bool alert = false;
+    Rigidbody2D rb;
     EnemyManager manager;
 
     void Awake() {
+        rb = GetComponent<Rigidbody2D>();
         manager = EnemyManager.Instance;
     }
 
     void Update() {
         if (alert) {
-            SearchPlayer();
+            AttackMode();
         }
     }
 
-    void SearchPlayer() {
-        
+    void AttackMode() {
+        Vector3 dir = target.transform.position - transform.position;
+        rb.velocity = new Vector2(dir.x, dir.y).normalized * moveSpeed;
     }
 
     void Attacked(Vector2 from) {
@@ -31,12 +36,17 @@ public class Enemy : MonoBehaviour {
     }
 
     void Knockback(Vector2 from) {
-        //TODO
+        rb.AddForce(-from.normalized * knockMult);
     }
 
     void Yell() {
         //anim
         manager.AlertNearby(this);
+    }
+
+    public void SeeHero(Hero hero) {
+        target = hero;
+        TurnAlert();
     }
 
     public void TurnAlert() {
---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+	void Awake () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+		
+	}
+
+    void CheckEnemy() {
+        
+    }
+}
