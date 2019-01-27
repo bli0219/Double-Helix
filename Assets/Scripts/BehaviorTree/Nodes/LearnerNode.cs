e8e5f73fee946b3c04946a96c59a66a67d8513fc
bli <bli@trinity.edu>
2019-01-27 15:37:38 -0600

    Learner node and things before it

---
+++ Assets/Scripts/BehaviorTree/Nodes/LearnerNode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MyBehaviorTree {
+	public class LearnerNode : ICompositeNode {
+
+		List<int> scores;
+		bool ticked = false;
+		int lastTicked = -1;
+
+		public LearnerNode (string name, ITreeNode[] children, BehaviorTree bt) : base(name, children, bt) {
+			scores = new List<int>(new int[children.Length]);
+		}
+		public LearnerNode (string name, BehaviorTree bt) : base(name, bt) { }
+
+		public override void Tick () {
+
+			if (scores == null) {
+				scores = new List<int>(new int[Children.Length]);
+			} else {
+				Debug.LogWarning("Scores not initialized");
+			}
+
+			if (!ticked) {
+				int largest = 0;
+				for (int i = 1; i != scores.Count; i++) {
+					if (scores[i] > scores[largest]) {
+						largest = i;
+					}
+				}
+				lastTicked = largest;
+				BehaviorTree.path.Push(Children[largest]);
+			} else {
+				if (BehaviorTree.lastStatus == NodeStatus.Success)
+					scores[lastTicked] += 1;
+				if (BehaviorTree.lastStatus == NodeStatus.Failure)
+					scores[lastTicked] -= 1;
+				BehaviorTree.Finish(BehaviorTree.lastStatus);
+			}
+		}
+	}
+}
